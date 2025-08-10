fc416806c10bd284e2326293dcc1a001600be729
Valmir Dornas <fernandocursos2021@gmail.com>
2025-08-10 12:09:04 -0300

    Configurações iniciais

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Container } from "@/components/container";
+
+export default function Dashboard(){
+    return(
+        <Container>
+            <h1>Página Dashboard</h1>
+        </Container>
+    );
+}
